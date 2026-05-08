cf01a4d74b35a7f5b0af13e17affef0c0403f977
triracle97 <triracle@gmail.com>
2026-05-08 23:08:59 +0700

    feat(web): tailwind v4 theme tokens + flicker prevention

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import './index.css';
 import './App.css';
 import 'katex/dist/katex.min.css';
 import './features/markdown/markdown.css';
